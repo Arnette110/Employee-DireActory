18bff925b9d7a8e56285b5431ec5a288305d4677
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-06 18:20:38 -0600

    init commit, currently unused

---
+++ src/components/search.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// function Search(props){
+//     return (
+//       <form>
+//         <div className="input-field">
+//           <input id="search" type="search" required />
+//           <label className="label-icon" for="search">
+//             <i className="material-icons">search</i>
+//           </label>
+//         </div>
+//       </form>
+//     );
+// }
+
+// export default Search;
