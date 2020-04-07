a54f336eaae24590b4f838d0d087eabf54f2af9e
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-06 18:20:48 -0600

    init commit

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="container">
+      <nav>
+        <div className="nav-wrapper grey">
+          <h3 className="center-align">C@rr0t Co.</h3>
+          
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
