5e6effe25d91fd61a85b1566af966b69febfa312
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-08 15:41:59 -0600

    removed container class

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,14 +2,13 @@ import React from "react";
 
 function NavBar() {
   return (
-    <div className="container">
+    
       <nav>
         <div className="nav-wrapper grey">
           <h3 className="center-align">C@rr0t Co.</h3>
-          
         </div>
       </nav>
-    </div>
+    
   );
 }
 
