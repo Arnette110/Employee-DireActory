d42f764821ef653662b4278bb0bc525f74d1ac6c
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-09 21:13:31 -0600

    import Employees as props rather than directly from Employees object, added sort button to first name column

--- src/components/table.js
+++ src/components/table.js
@@ -1,50 +1,35 @@
 import React from "react";
-// import MaterialTable from "material-table";
-import Employees from "../utils/employees";
 
 
 function EmployeeTable(props) {
-    
   return (
-    <div>
-      {/* <MaterialTable
-        columns={[
-          { title: "User ID", field: "id" },
-          { title: "First Name", field: "firstName" },
-          { title: "Last Name", field: "lastName" },
-          { title: "E-mail", field: "email" },
-          { title: "Phone", field: "phone" },
-          { title: "Department", field: "department" },
-        ]}
-        data={Employees}
-        title="Employee Directory"
-      /> */}
-      <div className="card">
-        <table>
-          <thead>
+    <div className="card">
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>
+              First Name <button className="btn grey darken-4" onClick={props.handleSortBtn}><i className="material-icons">unfold_more</i></button>
+            </th>
+            <th>Last Name</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        {props.Employees.map((e) => (
+          <tbody key={e.id}>
             <tr>
-              <th>User ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>E-mail</th>
-              <th>Phone</th>
-              <th>Department</th>
+              <td>{e.id}</td>
+              <td>{e.firstName}</td>
+              <td>{e.lastName}</td>
+              <td>{e.email}</td>
+              <td>{e.phone}</td>
+              <td>{e.department}</td>
             </tr>
-          </thead>
-          {Employees.map((e) => (
-            <tbody>
-              <tr key={e.id}>
-                <td>{e.id}</td>
-                <td>{e.firstName}</td>
-                <td>{e.lastName}</td>
-                <td>{e.email}</td>
-                <td>{e.phone}</td>
-                <td>{e.department}</td>
-              </tr>
-            </tbody>
-          ))}
-        </table>
-      </div>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
