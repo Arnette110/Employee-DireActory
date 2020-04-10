3b691ae81ae3fbad3a035c0f1d48651fdb902fa0
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-09 21:11:57 -0600

    search by first name working, filter by first name working

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,56 +2,61 @@
 import React, { useState } from "react";
 import Employees from "../../utils/employees";
 import SearchForm from "../../components/SearchForm/search";
+import EmployeeTable from "../../components/table";
+import NavBar from "../../components/navbar";
 
 export default function Search() {
   const [search, setSearch] = useState("");
-    const[employee, setEmployee] = useState("");
-    const[error, setError] = useState("")
-    //  const findEmployees = Employees.filter((e) => {
-    //    return e.firstName.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-    //  });
-    // useEffect(() => {
-    //     Employees.Employees(search)
-    //         .then((res) => {
-    //     if (res.data.length === 0) {
-    //       throw new Error("No results found.");
-    //     }
-    //     if (res.data.status === "error") {
-    //       throw new Error(res.data.message);
-    //     }
-        
-    //     setError("");
-    //   })
-    //   .catch((err) => setError(err.message));
-    //     }, [search])
-    
-  
+  const [employee, setEmployee] = useState("");
+  const [sortedField, setSortedField] = useState("");
+  const [error, setError] = useState("");
+  const [sortConfig, setSortConfig] = useState("");
+  const findEmployees = Employees.filter((e) => {
+    return e.firstName.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+
   const handleInputChange = (event) => {
     setSearch(event.target.value);
-    const findEmployees = Employees.filter((e) => {
-      return e.firstName.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-    });
-    console.log(...findEmployees);
+    // setEmployee(findEmployees)
   };
 
-  const handleFormSubmit = (event) => {
-      event.preventDefault();
-    setSearch(event.target.value);
-    const findEmployees = Employees.filter((e) => {
-      return e.firstName.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-    });
-    console.log(...findEmployees);
+  const handleSortBtn = (key) => {
+    let direction = 'ascending';
+    if (
+      sortedField &&
+      sortedField.key === key &&
+      sortedField.direction === 'ascending'
+    ) {
+      direction = 'descending';
+    }
+    setSortedField({ key, direction });
+  
+    // event.preventDefault()
+   
+    if(sortedField !== null) {
+      Employees.sort((a, b) => {
+        if (a.firstName < b.firstName) {
+          return sortedField.direction === "ascending" ? -1 : 1;
+        }
+        if (a.firstName > b.firstName) {
+          return sortedField.direction === "ascending" ? 1 : -1;
+        } 
+        return 0;
+      });
+    }
   };
+
+    
+  
 
   return (
     <div>
-      <SearchForm
-        handleInputChange={handleInputChange}
-        handleFormSubmit={handleFormSubmit}
-        results={search}
-        // findEmployees={findEmployees}
+      <NavBar />
+      <SearchForm handleInputChange={handleInputChange} results={search} />
+      <EmployeeTable
+        Employees={findEmployees}
+        handleSortBtn={handleSortBtn}
       />
     </div>
   );
 }
-
