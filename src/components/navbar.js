2a4432b15ef7d711027f691104f3866691a46e14
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-11 14:47:45 -0600

    changed some styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,17 +2,22 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav className="nav-extended grey darken-4">
-      <div className="nav-wrapper  valign-wrapper center-align">
-        <h3 className="center-align">C@rr0t Co.</h3>
-      </div>
-      <div className="nav-content">
-        <h4 className="center-align">Employee Directory</h4>
-      </div>
-      <div>
-        <br />
-      </div>
-    </nav>
+    <div>
+      <nav className="nav-extended grey darken-4">
+        <div>
+          <br />
+        </div>
+        <div className="nav-wrapper center-align">
+          <h3 className="center-align">C@rr0t Co.</h3>
+        </div>
+        <div className="nav-content">
+          <h4 className="center-align">Employee Directory</h4>
+        </div>
+        <div>
+          <br />
+        </div>
+      </nav>
+    </div>
   );
 }
 
